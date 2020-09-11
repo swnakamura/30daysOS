e0c7e338f8a0f4d23d3099ea2001074b944162ad
woodyZootopia <piequalsabout314159@gmail.com>
2020-09-11 18:30:52 +0900

    Show color lines

--- src/lib.rs
+++ src/lib.rs
@@ -21,12 +21,17 @@ fn show_white(i: u32) {
     *ptr = a
 }
 
+fn show_color(i: u32) {
+    let ptr = unsafe { &mut *(i as *mut u32) };
+    *ptr = i & 0x0f;
+}
+
 #[no_mangle]
 #[start]
 pub extern "C" fn haribote_os() -> ! {
     // 本にある通り、0xa0000から0xaffffまで描画
     for i in 0xa0000..0xaffff {
-        show_white(i);
+        show_color(i);
     }
     loop {
         hlt()
