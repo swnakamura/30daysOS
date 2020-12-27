7343b88dff0e645fbf30f92e9e0bc7457a4782dd
woodyZootopia <piequalsabout314159@gmail.com>
2020-12-27 21:06:32 +0900

    Print error code on double fault

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -38,9 +38,12 @@ mod handler {
 
     pub extern "x86-interrupt" fn double_fault_handler(
         stack_frame: &mut InterruptStackFrame,
-        _error_code: u64,
+        error_code: u64,
     ) -> ! {
-        panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+        panic!(
+            "EXCEPTION: DOUBLE FAULT\n{:#?}, {:#?}",
+            stack_frame, error_code
+        );
     }
 
     pub extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: &mut InterruptStackFrame) {
